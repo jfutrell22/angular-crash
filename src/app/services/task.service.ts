a68d115ae82fdd360a80d9810b689157ed285f60
John <jrfutrell@my.waketech.edu>
2024-07-17 16:32:18 -0400

    save tasks updated

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -24,8 +24,13 @@ export class TaskService {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url);
   }
+
   updateTaskReminder(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.put<Task>(url, task, httpOptioins)
+  }
+
+  addTask(task: Task):Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptioins);
   }
 }
