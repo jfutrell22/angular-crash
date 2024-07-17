766dce0868b8951b38d96395c0bdff2ee73d01b5
John <jrfutrell@my.waketech.edu>
2024-07-16 21:24:00 -0400

    added button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-crash';
+export class AppComponent {  
 }
